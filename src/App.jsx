ddaf7215a1ca4aecbd274b28e334fb39c378d023
FabianCasti <fcastillop@unicesar.edu.co>
2022-08-11 23:07:24 -0500

    FinalProject

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+import './ProductInf.css'
+import './styles/home.css'
+import './styles/Login.css'
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import { Home, LogIn, ProductDetails, Purchases, ProtectedRoutes } from './pages/index'
+import Footer from './Components/Footer';
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  return (
+    <HashRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/product/:id' element={<ProductDetails />} />
+      <Route element={<ProtectedRoutes />}>
+        <Route path='/purchases' element={<Purchases />} />
+      </Route>
+      <Route path='/LogIn' element={<LogIn />} />      
+    </Routes>
+  </HashRouter>
+  )
+}
+
+export default App
